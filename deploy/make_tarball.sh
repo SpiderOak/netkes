7b2cc6a13a5bc6ecdb6380683f726a6cda35f579
bdzim <benjamin.zimmerman@gmail.com>
2013-07-09 16:04:09 -0400

    properly bundle all of netkes

--- deploy/make_tarball.sh
+++ deploy/make_tarball.sh
@@ -19,42 +19,12 @@ rm $deploy_dir/openmanage.tar.bz2
 rm -r $buildit_dir
 mkdir $buildit_dir
 
-# Setup the python packages in the tarball.
-# included_packages="bin etc lib sql"
-
-# for package in $included_packages; do
-#     mkdir $buildit_dir/$package
-#     cp $source_dir/$package/*.py $buildit_dir/$package/ 2> /dev/null
-#     cp $source_dir/$package/*.sh $buildit_dir/$package/ 2> /dev/null
-# done
-
 mkdir $buildit_dir/git
-cp -r $source_dir $buildit_dir/git
+cp -r $source_dir/!(git) $buildit_dir/git
 
-: <<'END'
 # Setup the base.
 mkdir $buildit_dir/bin
 cp $source_dir/bin/*.{sh,py} $buildit_dir/bin 2> /dev/null
-
-# Copy libraries
-mkdir $buildit_dir/netkes
-for package in `ls $source_dir/netkes`; do
-    mkdir -p $buildit_dir/netkes/$package
-    cp $source_dir/netkes/$package/*.py $buildit_dir/netkes/$package 2> /dev/null
-done
-
-# Copy LDAP over if necessary.
-if [ $4 == "ldap" ]; then    
-    mkdir $buildit_dir/netkes/account_mgr/user_source
-    cp $source_dir/netkes/account_mgr/user_source/*.py $buildit_dir/netkes/account_mgr/user_source
-fi
-
-# Copy over the django project
-cp -r $source_dir/django $buildit_dir
-
-# Setup the SQL package
-mkdir $buildit_dir/sql
-cp $source_dir/sql/*.sql $buildit_dir/sql
 
 # Package the configuration files.
 included_management="openmanage_defaults apt_list py_list agent_config.json.sample"
@@ -62,7 +32,6 @@ mkdir $buildit_dir/etc
 for file in $included_management; do
     cp $source_dir/etc/$file $buildit_dir/etc
 done
-END
 
 # Set the brand in the configuration
 echo "OPENMANAGE_BRAND=$3" > $buildit_dir/etc/brand
