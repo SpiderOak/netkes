b9e14bc3e94e3c543518a3e3dc2a253c84691a04
bdzim <benjamin.zimmerman@gmail.com>
2013-08-27 15:44:16 -0400

    added package upgrades back in

--- upgrade/scripts/2013-11-08_blue1.3.sh
+++ upgrade/scripts/2013-11-08_blue1.3.sh
@@ -5,3 +5,8 @@ PYTHONPATH=/opt/openmanage/django:$PYTHONPATH
 python /opt/openmanage/django/apps/blue_management/blue_mgnt/permissions.py
 
 python /opt/openmanage/django/omva/manage.py syncdb --noinput
+
+sudo apt-get remove -y python-django
+
+sudo pip install --upgrade /opt/openmanage/upgrade/resources/pip-1.4.1.tar.gz
+sudo pip install /opt/openmanage/upgrade/resources/Django-1.5.5.tar.gz
