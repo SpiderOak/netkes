e84106055e1965002e03fac5f9029932b01fea1e
Paul Kenyon <rixoff@gmail.com>
2013-12-02 06:49:52 -0600

    Updates to the upgrade scripts

--- django/setup_git.sh
+++ django/setup_git.sh
@@ -1,33 +1,36 @@
 #!/bin/bash
 
 set -e
-set -x
+#set -x
 set -o pipefail
 
-pushd ${1:?}/django
+pushd ${1:?}/django > /dev/null
 
+echo
+echo Downloading other git modules:
+echo
 git clone https://github.com/jimfunk/django-postgresql-netfields.git
 
 mkdir apps
-pushd apps
+pushd apps > /dev/null
 
 git clone https://spideroak.com/dist/blue_management.git
 git clone https://spideroak.com/dist/so_common.git
-pushd blue_management
+pushd blue_management > /dev/null
 
+echo Initializing submodules...
 git submodule init
 git submodule update
 
 ln -s templates/base ../so_common/templates/base
 
-popd # blue_management
-popd #apps
+popd > /dev/null # blue_management
+popd > /dev/null #apps
 
-pwd
 # Setup the static content
 mkdir static
 
 ln -s /opt/openmanage/django/apps/so_common/static static/blue_common
 
-popd
+popd > /dev/null
 
