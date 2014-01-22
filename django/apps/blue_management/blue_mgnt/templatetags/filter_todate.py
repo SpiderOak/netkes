52e7f3ced45de2196ec9c6e15789406132629eda
Bennyoak <benny@spideroak.com>
2014-01-22 16:08:49 -0600

    Added custom template tag filter 'todate'. Updated user_detail with said filter.

---
+++ django/apps/blue_management/blue_mgnt/templatetags/filter_todate.py
@@ -0,0 +1,16 @@
+# Django custom template filter.
+# Todate takes a JSON timestamp and converts it
+# to python friendly human readable time.
+# Usage: {% load filter_todate %} - placed in every template you want to use the filter.
+#        Then {{ <var>|todate }} where applicable.
+
+from django import template
+import datetime
+
+register = template.Library()
+
+@register.filter
+
+def todate(obj):
+    return datetime.datetime.fromtimestamp(obj)
+
