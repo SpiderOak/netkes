dfdec3647cb857fe68f2e2d5417b2f3bce18f57a
Bennyoak <benny@spideroak.com>
2014-01-31 17:43:18 -0600

    Added all the things. Everything is WIP, but almost there.

--- django/apps/blue_management/blue_mgnt/static/js/console.js
+++ django/apps/blue_management/blue_mgnt/static/js/console.js
@@ -1,8 +1,37 @@
 $(function() {
+
+    // Workings for the modal wrapper and widgets
+
     $('#add-user').click(function(){
-        $('.widget-add-user').show();
         $('.modal-wrapper').show();
     });
 
+    $('#option-add-user button').click(function(){
+            $('.widget-add-user-option').hide();
+            $('.widget-add-user').show();
+    });
 
+    $('h2.page-header .actions').click(function() {
+        $('.widget-add-user').hide()
+        $('.widget-add-user-option').show();
+        $('.modal-wrapper').hide();
+    });
+
+
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie != '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+var csrftoken = getCookie('csrftoken');
 });
