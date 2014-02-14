15240d9fb8d0fed30c1555179dce8ff77bdff8c3
bdzim <benjamin.zimmerman@gmail.com>
2014-02-13 19:14:48 -0500

    setup version 2.0

--- django/setup_git.sh
+++ django/setup_git.sh
@@ -8,17 +8,10 @@ pushd ${1:?}/django
 
 git clone https://github.com/jimfunk/django-postgresql-netfields.git
 
-pushd apps
-
-git clone https://spideroak.com/dist/so_common.git
-
-popd #apps
-
 pwd
 # Setup the static content
 mkdir static
 
-ln -s /opt/openmanage/django/apps/so_common/static static/blue_common
 ln -s /opt/openmanage/django/apps/blue_management/blue_mgnt/static static/blue_mgnt
 
 popd
