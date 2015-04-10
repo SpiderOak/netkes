5f60501efeca3058cf87080e0fba3ab318a42871
brian-emery <brian.emery.smith@gmail.com>
2015-04-10 12:14:41 -0400

    change directory_agent logrotate trigger to size

--- upgrade/2013-09-10_update_logging.sh
+++ upgrade/2013-09-10_update_logging.sh
@@ -14,22 +14,6 @@ mkdir -p /var/log/omva/directory_agent
 mkdir -p /var/log/omva/admin_console
 mkdir -p /var/log/omva/openmanage
 
-# directory_agent won't magically rotate as it's not via runit.
-if [ ! -f /etc/logrotate.d/directory_agent ]; then
-    cat > /tmp/rotate.om <<EOF
-/var/log/omva/directory_agent {
-  rotate 10
-  weekly
-  compress
-  missingok
-  notifempty
-}
-EOF
-
-    install /tmp/rotate.om /etc/logrotate.d/directory_agent
-    rm /tmp/rotate.om
-fi
-
 for SERVICE in openmanage admin_console; do
     mkdir -p /etc/service/$SERVICE/log
 
