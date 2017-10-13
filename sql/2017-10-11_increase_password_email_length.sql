bc39eeaecc23f5bc842252d364ddc4d1619f4659
bdzim <benjamin.zimmerman@gmail.com>
2017-10-13 09:31:43 -0500

    increase the email length in the users table

--- sql/2017-10-11_increase_password_email_length.sql
+++ sql/2017-10-11_increase_password_email_length.sql
@@ -3,4 +3,6 @@ begin;
 alter table passwords alter column email type varchar(150);
 alter table passwords owner to admin_console;
 
+alter table users alter column email type varchar(150);
+
 commit;
